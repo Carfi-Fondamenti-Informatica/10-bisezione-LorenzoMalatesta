ad9bc62d039877ef747a893ad20be131ff2befc4
LorenzoMalatesta <100367512+LorenzoMalatesta@users.noreply.github.com>
2022-05-17 16:53:53 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
+float f (float y) {
+    return y*y * cos(y) + 1;
+}
+
 int main() {
-   return 0;
+    float a=0, b=0, err=0, x=0;
+    while (true) {
+        cout << "inserire gli estremi" << endl;
+        cin >> a >> b;
+        if (f(a)*f(b)<0) {
+            break;
+        }
+    }
+
+    while (true) {
+        x = (a+b)/2;
+        if (f(x) != 0) {
+            if (f(a) * f(b) < 0) {
+                b = x;
+            } else {
+                a = x;
+            }
+            err = abs((b - a) / 2);
+            if (err < 10 * exp(6)) {
+                break;
+            }
+
+        }
+    }
+
+    cout << x << endl;
+    cout << f(x) << endl;
+
+    return 0;
 }
