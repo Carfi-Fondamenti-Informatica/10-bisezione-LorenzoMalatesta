820aa37a7d0d7c3de11c40a75812a2aae7def808
LorenzoMalatesta <100367512+LorenzoMalatesta@users.noreply.github.com>
2022-05-17 17:48:25 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,38 +2,31 @@
 #include <math.h>
 using namespace std;
 
-float f (float y) {
-    return y*y * cos(y) + 1;
-}
-
 int main() {
-    float a=0, b=0, err=0, x=-1;
-    while (true) {
+    float a, b, fa, fb, x, fx, err;
+    do {
         cout << "inserire estremi";
         cin >> a >> b;
-        if (f(a)*f(b)<0) {
+        fa = a * a * cos(a) + 1;
+        fb = b * b * cos(b) + 1;
+    } while (fa*fb >= 0);
+
+    do {
+        x = (a+b)/2;
+        fx = x * x * cos(x) + 1;
+        if (fx == 0) {
             break;
         }
-    }
-
-    while (true) {
-        a = (a+b)/2;
-        x = a;
-        if (f(x) != 0) {
-            if (f(a) * f(b) < 0) {
-                b = x;
-            } else {
-                a = x;
-            }
-            err = abs((b - a) / 2);
-            if (err < 10 * exp(6)) {
-                break;
-            }
+        if (fa*fx<0) {
+            b=x;
+        } else {
+            a=x;
         }
-    }
+        err = abs((b - a) / 2);
+    } while (err>=1e-6);
 
     cout << x;
-    cout << f(x);
+    cout << fx;
 
     return 0;
 }
